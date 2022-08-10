cea24f1efe163463dc057daefb74c88a5141dcf9
Alex7-8 <71002189+Alex7-8@users.noreply.github.com>
2022-08-10 00:40:44 -0600

    V0.0.8
    Correcciones

--- PHP/ActualizarRegistro.php
+++ PHP/ActualizarRegistro.php
@@ -4,10 +4,17 @@ $id = $_GET['id'];
 $nom = $_GET['nombre'];
 $ape = $_GET['apellido'];
 $msg = $_GET['mensaje'];
+
 $actualizar ="update usuarios set nombre = '$nom', apellido = '$ape', mensaje = '$msg' where id = $id";
 
 $actualiza = mysqli_query($conn, $actualizar);
 
+if($conn->query($sql) === TRUE){
+    $response["status"] = 200;
+    $response["message"] = "Registro actualizado";
+}else{
+    $response["message"] = "Hubo un error";
+}
 $conn -> close();
 
 
